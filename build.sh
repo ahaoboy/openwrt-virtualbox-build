557f7beb38ebd39f8e5385276f9819780e5ede15
ahaoboy <504595380@qq.com>
2025-11-06 21:30:47 +0800

    parted

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ for file in *.img; do
   fdisk -l $file
 
   VBoxManage convertfromraw --format VDI $file $base.vdi
-  # VBoxManage modifyhd --resize 512 $base.vdi
+  VBoxManage modifyhd --resize 1024 $base.vdi
 
 done
 
