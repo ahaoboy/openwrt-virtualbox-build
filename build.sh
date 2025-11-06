0d55774418d716a94e148edc8551e59ca6fc63b8
ahaoboy <504595380@qq.com>
2025-11-06 21:30:31 +0800

    parted

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 URLS=(
-  "https://downloads.openwrt.org/releases/24.10.4/targets/x86/64/openwrt-24.10.4-x86-64-generic-ext4-combined.img.gz"
+  "https://downloads.openwrt.org/releases/24.10.4/targets/armsr/armv7/openwrt-24.10.4-armsr-armv7-generic-squashfs-combined-efi.img.gz"
+  "https://downloads.openwrt.org/releases/24.10.4/targets/armsr/armv8/openwrt-24.10.4-armsr-armv8-generic-squashfs-combined-efi.img.gz"
+  "https://downloads.openwrt.org/releases/24.10.4/targets/x86/64/openwrt-24.10.4-x86-64-generic-squashfs-combined-efi.img.gz"
+
+  # "https://downloads.openwrt.org/releases/24.10.4/targets/x86/generic/openwrt-24.10.4-x86-generic-generic-squashfs-combined.img.gz"
+  # "https://downloads.openwrt.org/releases/24.10.4/targets/armsr/armv7/openwrt-24.10.4-armsr-armv7-generic-squashfs-rootfs.img.gz"
+  # "https://downloads.openwrt.org/releases/24.10.4/targets/armsr/armv8/openwrt-24.10.4-armsr-armv7-generic-squashfs-rootfs.img.gz"
+  # "https://downloads.openwrt.org/releases/24.10.4/targets/x86/64/openwrt-24.10.4-x86-64-generic-ext4-combined.img.gz"
   # "https://downloads.openwrt.org/releases/24.10.4/targets/x86/64/openwrt-24.10.4-x86-64-generic-ext4-combined-efi.img.gz"
 )
 
